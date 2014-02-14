ea3d518694689a9d24c4604c968fe883d4fa4bb8
Kevin Yeh <null>
2014-02-14 13:33:29 -0800

    Add the user authentication model

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe User do
   it { should respond_to :email }
   it { should respond_to :password_hash }
+  it { should respond_to :password }
   it { should have_many :location_users }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password_hash }
@@ -15,5 +16,25 @@ describe User do
 
     user = User.new(email: 'k@fed.com', password_hash: 'woops' )
     expect(user).not_to be_valid
+  end
+
+  describe 'user authentication' do
+    before do
+      @user = User.new(email: 'lebron@miamiheat.com')
+      @user.password="banana"
+      @user.save
+    end
+
+    after do
+      @user.destroy
+    end
+
+    it 'should authorize when correct password given' do
+      expect(@user.password=="banana").to be_true
+    end
+
+    it 'should reject with incorrect password' do
+      expect(@user.password=="fake").to be_false
+    end
   end
 end
